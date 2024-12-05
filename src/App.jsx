324ff44d8a7902341b648d6d05504b694cb82146
Joshua Crass <joshua.crass@gmail.com>
2024-12-05 10:54:12 -0800

    added new header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <h1>React From Scratch</h1>
     </div>
   );
