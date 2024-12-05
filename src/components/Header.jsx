17389419bbaab77a78a78253bf92a6c19c462393
Joshua Crass <joshua.crass@gmail.com>
2024-12-05 11:12:10 -0800

    added emotion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,33 @@
+import styled from "@emotion/styled";
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #000;
+  color: #fff;
+  padding: 20px;
+`;
+
+const Navbar = styled.nav`
+  display: flex;
+  gap: 20px;
+`;
+
+const StyledLink = styled.span`
+  font-size: 20px;
+`;
+
 const Header = () => {
   return (
-    <div>
+    <StyledHeader>
       <h1>Header</h1>
-    </div>
+      <Navbar>
+        <StyledLink>Home</StyledLink>
+        <StyledLink>About</StyledLink>
+        <StyledLink>Login</StyledLink>
+      </Navbar>
+    </StyledHeader>
   );
 };
 
